3e1646244ec97cc059a533ca30b7ecff667c1c3c
Dragon867 <hamza.jadoon2007@gmail.com>
2024-09-16 20:19:10 +0500

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,21 @@
+name = "Hamza"
+age = 17
+is_student = True
+weight = 50.12
+
+print("Name :",name)
+print("Data type of name is:",type(name))
+print("age",age)
+print("Data type of age is:",type(age))
+print("Student:",is_student)
+print("Data type of student is:",type(is_student))
+print("Weight",weight)
+print("Data type of weight is:",type(weight))
+
+print("/naftertypecasting")
+age = str(age)
+print("age",age)
+print("Data type of age is:",type(age))
+weight = int(weight)
+print("Weight",weight)
+print("Data type of weight is:",type(weight))
